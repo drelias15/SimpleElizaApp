be3c31a6c152548b24c15e29a73fa5578d5a5d1a
drelias15 <drelias15@gmail.com>
2018-07-17 17:04:42 -0400

    Updated

--- src/SimpleElizaApp.java
+++ src/SimpleElizaApp.java
@@ -9,13 +9,13 @@ public class SimpleElizaApp {
         String question;
 
         System.out.println("Good day. What is your problem? Enter your response here or Q to quit:");
-        question = keyboard.nextLine().toLowerCase();
+        question = keyboard.nextLine();
 
 
-        while (!question.equals("q") && !question.equals("i am feeling great") ) {
+        while (!question.equalsIgnoreCase("q") && !question.equalsIgnoreCase("i am feeling great") ) {
 
             System.out.println(question);
-            question = keyboard.nextLine().toLowerCase();
+            question = keyboard.nextLine();
         }
         System.out.println("Thank you.");
     }

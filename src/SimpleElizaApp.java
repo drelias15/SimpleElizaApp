57d74151879394ec0406248b9cfca26d02801a0f
drelias15 <drelias15@gmail.com>
2018-07-17 16:52:33 -0400

    first commit

---
+++ src/SimpleElizaApp.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class SimpleElizaApp {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        String question;
+
+        System.out.println("Good day. What is your problem? Enter your response here or Q to quit:");
+        question = keyboard.nextLine().toLowerCase();
+
+
+        while (!question.equals("q") && !question.equals("I am feeling great") ) {
+
+            System.out.println(question);
+            question = keyboard.nextLine().toLowerCase();
+        }
+        System.out.println("Thank you.");
+    }
+}
